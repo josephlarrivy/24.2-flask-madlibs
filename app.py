cb0838797bc3a190b84f0532b46c9cda796314b2
josephlarrivy <joseph.larrivy@gmail.com>
2022-09-17 11:08:12 -0500

    assignment complete

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, render_template
+from stories import story
 
 app = Flask(__name__)
 
+
+
+
+
 @app.route('/')
 def form():
-    return render_template('form.html')
+    prompts = story.prompts
+    return render_template('form.html', prompts=prompts)
 
-
+@app.route('/submitted')
+def submitted():
+    text = story.generate(request.args)
+    return render_template('submitted.html', text=text)
 
 
 
